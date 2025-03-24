9e942b228ee924e785119e1b01b136be497a057c
MuhammadMakhfud <makhfud1107@gmail.com>
2025-03-24 13:53:12 +0800

    fud

---
+++ src/PZEM003_Fud.h
@@ -0,0 +1,29 @@
+#ifndef PZEM003_FUD_H
+#define PZEM003_FUD_H
+
+#include <SoftwareSerial.h>
+
+class PZEM003_Fud {
+public:
+    PZEM003_Fud(uint8_t rxPin, uint8_t txPin, uint8_t address = 0x01);
+    void begin(uint32_t baudRate);
+    void update();
+    void resetEnergy();
+    float getVoltage();
+    float getCurrent();
+    float getPower();
+    float getEnergy();
+
+private:
+    SoftwareSerial pzemSerial;
+    uint8_t address;
+    byte buffer[100];
+    byte* ptr;
+    float voltage, current, power, energy;
+
+    uint16_t calcCRC(byte* data, byte length);
+    void readData();
+    void sendCommand(byte* command, size_t length);
+};
+
+#endif
